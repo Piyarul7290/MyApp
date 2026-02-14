aaec44e222f69820bd7f4007d5ffe763561db2ca
Piyarul7290 <piyarusekh7290@gmail.com>
2026-02-14 22:58:12 +0530

    Update HealthCheck01.py

--- HealthCheck01.py
+++ HealthCheck01.py
@@ -1,15 +1,13 @@
 #python project
 
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Health Check Okay"
-
-@app.route("/health")
 def health_check():
-    return "Ok", 200
+    return "Application is Healthy!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
